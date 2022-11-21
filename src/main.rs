5bacfdad12d334668f87899f04318a4ac38dc4f7
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-11-21 19:04:04 +0900

    add

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use rsa_test::{create_file, decrypt_file, encrypt_file, read_key_files, Key, Key
 
 fn main() {
     println!("rsa알고리즘을 구현");
-
-    let matches = App::new("rsa알고리즘을 구현");
-
     let test = KeySet::new();
     println!("{:?}", test)
+}
+#[cfg(test)]
+mod test {
+    #[test]
+
+    fn test() {}
 }

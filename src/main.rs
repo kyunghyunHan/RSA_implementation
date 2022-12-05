5fec79de116cc41c494623ccfa0066b65eac4b10
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-12-05 12:55:32 +0900

    ㅁㅇㅇ

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use rsa_test::{create_file, decrypt_file, encrypt_file, read_key_files, Key, Key
 fn main() {
     println!("rsa알고리즘을 구현");
     let test = KeySet::new();
-    println!("{:?}", test)
+    println!("{:?}", test);
+
+    static KEY_FILE_NAME: &'static str = "key";
+    static PUBLIC_KEY_SUFFIX: &'static str = "pub";
 }
 #[cfg(test)]
 mod test {
